50bd4321da8a0156173ff7811a3e3040b09cff26
sam2much96 <sam2much96@gmail.com>
2023-04-15 07:00:35 +0100

    modified:   DevHelper.py

--- DevelopersHelper/DevHelper.py
+++ DevelopersHelper/DevHelper.py
@@ -1,6 +1,26 @@
 import json
 import hashlib
 
-def GenerateTEAL(filename : str):
-    #...
-    return ""  #should return your generated TEAL code
+def GenerateTEAL(filename: str) -> str:
+
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    # Extract necessary data from the JSON file
+    call_configs = []
+    for interface in data['Interfaces']:
+        for method in interface['methods']:
+            call_configs.extend(method['call_config'])
+
+    # Use the extracted data to build the TEAL smart contract template string
+    template = f"""# TEAL smart contract template generated from {filename}
+
+
+    {json.dumps(call_configs, indent=4)}
+
+    """
+    return template
+
+
+if __name__ == "__main__":
+    print(GenerateTEAL("Test1.json"))
